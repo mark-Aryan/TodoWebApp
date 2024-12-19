ef58376fa86ab32c2c55a70af3a8e0157b12ab6c
Aryan Upadhyay <81193071+mark-Aryan@users.noreply.github.com>
2024-12-20 02:46:18 +0530

    Update WebTodo.py

--- WebTodo.py
+++ WebTodo.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import functions
 
+# Initialize todos
 todos = functions.get_todos()
-
 
 def add_todo():
     new_todo = st.session_state["new_todo"] + "\n"
@@ -10,18 +10,22 @@ def add_todo():
     functions.write_todos(todos)
     st.session_state["new_todo"] = ""
 
+# Initialize session state
+if "new_todo" not in st.session_state:
+    st.session_state["new_todo"] = ""
+
 st.title("WebTodoGUI")
 st.subheader("This is my Todo app.")
 
+# Display todos with unique keys
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
+    checkbox = st.checkbox(todo, key=f"{todo}_{index}")
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
-        del st.session_state[todo]
+        del st.session_state[f"{todo}_{index}"]
         st.experimental_rerun()
 
 st.text_input(label="", placeholder="Add new todo...", on_change=add_todo, key="new_todo")
-
 
 st.info("Created by: Aryan Kumar Upadhyay")
